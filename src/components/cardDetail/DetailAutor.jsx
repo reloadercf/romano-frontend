801913fe285a92ec363af193cfdc406dc14241d8
EDUARDO BENITEZ <eduardobenitez@MacBook-Pro-de-EDUARDO.local>
2018-10-12 13:10:49 -0500

    correcion blur de imagenes

--- src/components/cardDetail/DetailAutor.jsx
+++ src/components/cardDetail/DetailAutor.jsx
@@ -8,7 +8,7 @@ export const DetailAutor=({autor})=>{
             <Row type="flex" justify="start" align="center" >
                 <Col md={8} sm={8} xs={24} >
                      <div className="card_autor_image">
-                           <img src={autor.foto} ></img>
+                           <img src={autor.foto}  alt={autor.nombre}></img>
                      </div>
                 </Col>
                 <Col md={16} sm={16} xs={24} >
@@ -16,11 +16,11 @@ export const DetailAutor=({autor})=>{
                           <h4>AUTOR: {autor.nombre}</h4>
                           <span>{autor.biografia}</span>
                           <p>{autor.intereses}</p>
-                          <div className="card_autor_info_social">
-                        <FontAwesomeIcon icon={faFacebookSquare} size="2x" className="icon_style" />
-                        <FontAwesomeIcon icon={faTwitterSquare} size="2x" className="icon_style" />
-                        <FontAwesomeIcon icon={faWhatsappSquare} size="2x" className="icon_style" />
-                    </div>
+                        <div className="card_autor_info_social">
+                            <FontAwesomeIcon icon={faFacebookSquare} size="2x" className="icon_style" />
+                            <FontAwesomeIcon icon={faTwitterSquare} size="2x" className="icon_style" />
+                            <FontAwesomeIcon icon={faWhatsappSquare} size="2x" className="icon_style" />
+                        </div>
                     </div>
                   
                 </Col>
